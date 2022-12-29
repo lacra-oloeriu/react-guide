8347947a78901adc093fee434ef3c902177e9820
Lacramioara <lacramioara.oloeriu@gmail.com>
2022-12-29 13:04:19 +0100

    fix css by bogdan

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     {
       id: "e1",
       title: "Toilet Paper",
-      anount: 94.12,
+      amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New Tv", anount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New Tv", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
-      anount: 294.67,
+      amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,9 +4,9 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
-      <div className="expense-item_description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">{props.amount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
