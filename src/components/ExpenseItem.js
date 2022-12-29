89283f8a7b5ad98eff7e675f59dbd8ba636190fc
Lacramioara <lacramioara.oloeriu@gmail.com>
2022-12-29 12:50:16 +0100

    lacra first css fix

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem () {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurence';
-    const expenseAmount = 294.67;
-
-    return (
-        <div className="expense-item">
-          <div>{expenseDate.toISOString()}</div>
-          <div className="expense-item_description">
-              <h2>{ expenseTitle}</h2>
-              <div className ="expense-item_price">{ expenseAmount}</div>
-          </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
