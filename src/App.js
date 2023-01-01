633bc099c171278dbe230af835d2daee58e910a0
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-01 15:00:46 +0100

    ussing some diffrent kind of function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 import React from "react";
 import Expenses from "./components/Expenses";
 
-function App() {
+const  App = () => {
   const expenses = [
     {
       id: "e1",
@@ -27,18 +27,18 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
-  //return (
-  // <div>
-  // <h2> Let's get started!</h2>
-  //  <Expenses items={expenses}/>
-  // </div>
+  //return React.createElement(
+  // "div",
+  // {},
+  // React.createElement("h2", {}, "Let's get started!"),
+  // React.createElement(Expenses, { items: expenses })
   //);
+  return (
+    <div>
+      <h2> Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from './Card';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
