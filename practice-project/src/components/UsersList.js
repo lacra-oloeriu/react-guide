a57ec394308b7fb24e9c033a51de54d41f6aaa57
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-10 13:11:40 +0100

    managin the list of users via State and adding a key

--- practice-project/src/components/UsersList.js
+++ practice-project/src/components/UsersList.js
@@ -4,10 +4,10 @@ import Card from "./Card";
 
 const UsersList = (props) => {
   return (
-    <Card className ={classes.users}>
+    <Card className={classes.users}>
       <ul>
         {props.users.map((user) => (
-          <li>
+          <li key={user.id}>
             {user.name} ({user.age} years old){" "}
           </li>
         ))}
