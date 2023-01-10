08477b44ec5eabd223d58797ec3e372c157a79a6
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-10 12:50:01 +0100

    adding aUser List component and some style

--- practice-project/src/App.js
+++ practice-project/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./components/AddUser";
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
