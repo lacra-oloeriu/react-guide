d3ec7f60b33609b995c42aadb185196a0c74dbb5
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-19 13:29:33 +0100

    managed Cart&Modal State

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,13 +1,25 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const shownCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  //shownCartHandler will be calld orever the button is click!!Inside de Header(passing a pointer at the function down to props)
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={shownCartHandler} />
       <main>
         <Meals />
       </main>
