7daea420b071a02f0d3714d719f25c59dffd58c8
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-23 08:55:21 +0100

    added complex reducer logic

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -5,46 +5,63 @@ import CartContext from "./cart-context";
 const defaultCartState = {
   items: [],
   totalAmount: 0,
-}
+};
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
-      }}
-      return defaultCartState; 
-}
-
-
-    //const existingCartItemIndex = state.items.findIndex(
-    //  (item) => item.id === action.item.id
-   // );
-   // const existingCartItem = state.items[existingCartItemIndex];
+    const exidtingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[exidtingCartItemIndex];
     //let updatedItem;
-    //let updatedItems;
+    let updatedItems;
 
-    //if (existingCartItem) {
-     // const updatedItem = {
-      //  ...existingCartItem,
-        //amount: existingCartItem.amount + action.item.amount,
-     // };
-      //updatedItems = [...state.items];
-      //updatedItems[existingCartItemIndex] = updatedItem;
-    //} else {
-     // updatedItems = state.items.concat(action.item);
-   // }
-  //}
-  
+    if (existingCartItem) {
+       const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[exidtingCartItemIndex] = updatedItem;
+    } else {
+      //updatedItem = { ...action.item };
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+//const existingCartItemIndex = state.items.findIndex(
+//  (item) => item.id === action.item.id
+// );
+// const existingCartItem = state.items[existingCartItemIndex];
+//let updatedItem;
+//let updatedItems;
+
+//if (existingCartItem) {
+// const updatedItem = {
+//  ...existingCartItem,
+//amount: existingCartItem.amount + action.item.amount,
+// };
+//updatedItems = [...state.items];
+//updatedItems[existingCartItemIndex] = updatedItem;
+//} else {
+// updatedItems = state.items.concat(action.item);
+// }
 //}
 
- // return defaultCartState; 
-//};
+//}
 
+// return defaultCartState;
+//};
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
