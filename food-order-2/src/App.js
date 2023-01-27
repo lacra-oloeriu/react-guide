414d3c3360dcec9dc7edc5e15a2557421755ffbc
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-27 06:05:55 +0100

    managed cart modal state

--- food-order-2/src/App.js
+++ food-order-2/src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const shownCartHandler = () => {
+    setCartIsShown(true);
+  };
+  //call when the cart button is clickt
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={shownCartHandler} />
       <main>
         <Meals />
       </main>
