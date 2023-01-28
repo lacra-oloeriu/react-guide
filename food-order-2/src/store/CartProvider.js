bd4c30f07324c289002408f2c7ddb895cc5cfd4a
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-28 15:46:48 +0100

    added reducer logic

--- food-order-2/src/store/CartProvider.js
+++ food-order-2/src/store/CartProvider.js
@@ -9,16 +9,35 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
-    const updateTotalAmount =
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-      items: updateItems,
-      totalAmount: updateTotalAmount,
-    };
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
   }
   return defaultCartState;
-};
+  }
+
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
