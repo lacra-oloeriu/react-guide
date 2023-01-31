7a41f4bdba23807d27e249ac8ffe21816b576932
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-01-31 05:23:33 +0100

    added Paragraph component

--- look-behind/src/App.js
+++ look-behind/src/App.js
@@ -2,21 +2,26 @@ import React, { useState } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
+  console.log("APP RUNNING");
+
   const togglePatagraphHandler = () => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
-  
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={togglePatagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
+
+//{showParagraph && <p>This is new!</p>}
 
 export default App;
