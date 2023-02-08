16a5e5cf0eefbac81a8f0aafa20eb68b3f081de3
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-02-08 11:11:21 +0100

    sended a http request

--- hooks-example/src/App.js
+++ hooks-example/src/App.js
@@ -13,7 +13,8 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+    
+        'https://react-http-7ceb0-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
       );
 
       if (!response.ok) {
