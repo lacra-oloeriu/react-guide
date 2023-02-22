1cbe539d37e790d47481d9bd6b8717e552b2073a
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-02-22 06:01:07 +0100

    added some styling

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -7,28 +7,30 @@ const Checkout = (props) => {
   };
 
   return (
-    <form onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor="name"> Your Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" />
-      </div>
-      <button type="button" onClick={props.onCancel}>
+    <form className={classes.form} onSubmit={confirmHandler}>
+    <div className={classes.control}>
+      <label htmlFor='name'>Your Name</label>
+      <input type='text' id='name' />
+    </div>
+    <div className={classes.control}>
+      <label htmlFor='street'>Street</label>
+      <input type='text' id='street' />
+    </div>
+    <div className={classes.control}>
+      <label htmlFor='postal'>Postal Code</label>
+      <input type='text' id='postal' />
+    </div>
+    <div className={classes.control}>
+      <label htmlFor='city'>City</label>
+      <input type='text' id='city' />
+    </div>
+    <div className={classes.actions}>
+      <button type='button' onClick={props.onCancel}>
         Cancel
       </button>
-      <button>Confirm</button>
-    </form>
+      <button className={classes.submit}>Confirm</button>
+    </div>
+  </form>
   );
 };
 
