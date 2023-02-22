a4e6b7c8aab7b96c6b002fdd8b35fde1e0901a8d
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-02-22 10:28:33 +0100

    added form validation

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -47,32 +47,42 @@ const Checkout = (props) => {
     }
 
     //Submit the cart data
-  };
+};
+    const nameControlClasses = `${classes.control} ${
+      formInputValidity.name ? "" : classes.invalid
+    }`;
+
+    const streetControlClasses = `${classes.control} ${
+        formInputValidity.street ? "" : classes.invalid
+      }`;
+
+      const postalCodeControlClasses = `${classes.control} ${
+        formInputValidity.postalCode ? "" : classes.invalid
+      }`;
+
+      const cityControlClasses = `${classes.control} ${
+        formInputValidity.city ? "" : classes.invalid
+      }`;
+ 
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div
-        className={`${classes.control} ${
-          formInputValidity.name ? "" : classes.invalid
-        }`}
+        className={nameControlClasses}
       >
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
         {!formInputValidity.name && <p>Please entere a valid name!</p>}
       </div>
       <div
-        className={`${classes.control} ${
-          formInputValidity.street ? "" : classes.invalid
-        }`}
+        className={streetControlClasses}
       >
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
         {!formInputValidity.street && <p>Please entere a valid street!</p>}
       </div>
       <div
-        className={`${classes.control} ${
-          formInputValidity.postalCode ? "" : classes.invalid
-        }`}
+        className={postalCodeControlClasses}
       >
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
@@ -81,9 +91,7 @@ const Checkout = (props) => {
         )}
       </div>
       <div
-        className={`${classes.control} ${
-          formInputValidity.city ? "" : classes.invalid
-        }`}
+        className={cityControlClasses}
       >
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
