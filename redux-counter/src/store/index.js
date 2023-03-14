b881bc19489998f01fd9e42b2bfc343fa70d1045
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-03-14 06:08:34 +0100

    added the auth slice and wrking wirh multiple slices

--- redux-counter/src/store/index.js
+++ redux-counter/src/store/index.js
@@ -1,11 +1,11 @@
 //import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -54,11 +54,29 @@ const counterSlice = createSlice({
 //return state;
 //};
 
+const initialAuthState = {
+  isAurhenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAurhenticated = true;
+    },
+    logout(state) {
+      state.isAurhenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {counter:counterSlice.reducer, auth : authSlice.reducer},
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 //dispaching the actions
 
 export default store;
