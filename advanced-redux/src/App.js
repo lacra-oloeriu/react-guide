dc80e18f7adce426f9336230fc4fedfd284b5df7
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-03-27 14:32:00 +0200

    using an action creator thunk

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -4,8 +4,9 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
+//import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData} from './store/cart-slice';
 
 let isInitial = true;
 
@@ -14,49 +15,15 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "panding",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://advanced-redux-e5858-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-      //const responseData = await response.json();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "succes",
-          title: "Succes!...",
-          message: "Sent  cart data successfully!",
-        })
-      );
-    };
-
-    if ( isInitial) {
+    if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!...",
-          message: "Sent  cart data faild!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
+
   return (
     <Fragment>
       {notification && (
