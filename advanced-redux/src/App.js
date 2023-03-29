5d9c0392d6274ed8ca4ffeabca40cbadeff1d101
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-03-29 14:58:18 +0200

    finised fatching logic

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -25,7 +25,10 @@ function App() {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- advanced-redux/src/store/cart-actions.js
+++ advanced-redux/src/store/cart-actions.js
@@ -20,8 +20,9 @@ export const fetchCartData = () => {
       dispatch(
         cartActions.replaceCart({
           items: cartData.items || [],
-          totalQuantity: cartData.totalQuantity, })
-      )
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +48,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://advanced-redux-e5858-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
