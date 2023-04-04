34fa94466a7c3c4ab06b3b9be5b95c25afe09f61
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-04 14:23:36 +0200

    navigating between pages with Links

--- react-routing/src/pages/Home.js
+++ react-routing/src/pages/Home.js
@@ -1,5 +1,15 @@
+//import {} from "react-router-dom";
+
 function HomePage() {
-  return <h1> My Home Page</h1>;
+  return (
+    <>
+    
+      <h1> My Home Page</h1>;
+      <p>
+        Go to <a href="/products">the list of products</a>.
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
