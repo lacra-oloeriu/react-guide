ef146cbb02d5463dfc2810eb8217c361536a1e9c
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-10 11:47:20 +0200

    added a root layout

--- react-routes/frontend/src/App.js
+++ react-routes/frontend/src/App.js
@@ -6,12 +6,14 @@
 //    - EventDetailPage
 //    - NewEventPage
 //    - EditEventPage
+// Done
 // 2. Add routing & route definitions for these five pages
 //    - / => HomePage
-//    - /events => EventsPage
+//    - /events => EventsPagegivet france
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+// Done
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -27,13 +29,20 @@ import Events from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
 import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <Events /> },
-  { path: "/events/:eventId", element: <EventDetail /> },
-  { path: "/events/new", element: <NewEvent /> },
-  { path: "/events/:eventId/edit", element: <EditEvent /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <Events /> },
+      { path: "events/:eventId", element: <EventDetail /> },
+      { path: "events/new", element: <NewEvent /> },
+      { path: "events/:eventId/edit", element: <EditEvent /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
