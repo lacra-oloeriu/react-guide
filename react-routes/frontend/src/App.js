bd20a8b85d375b0d716abf6be3cc37b2c8190184
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-11 13:06:33 +0200

    output the ID of the selectet event on the EventDetailPage

--- react-routes/frontend/src/App.js
+++ react-routes/frontend/src/App.js
@@ -22,6 +22,7 @@
 // Done
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
+// Done
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
--- react-routes/frontend/src/pages/EventDetail.js
+++ react-routes/frontend/src/pages/EventDetail.js
@@ -1,6 +1,14 @@
+import { useParams } from "react-router-dom";
+
 function EventDetail() {
-    return  <h1>Event Detail Page</h1>;
-  }
-  
-  export default EventDetail;
-  
+  const params = useParams();
+  return (
+    <>
+      {" "}
+      <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
+}
+
+export default EventDetail;
