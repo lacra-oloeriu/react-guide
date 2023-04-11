4320aa847423188087b0f47819f0438b3a2f5a74
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-11 07:01:13 +0200

    output a list of dummy events to the EventsPage

--- react-routes/frontend/src/pages/Events.js
+++ react-routes/frontend/src/pages/Events.js
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Anothe event",
+  },
+];
+
 function Events() {
-    return  <h1>Events Page</h1>;
-  }
-  
-  export default Events;
-  
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Events;
