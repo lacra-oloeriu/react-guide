e0ab4c0451a17395dc55adbada59442949a71b87
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-11 06:23:21 +0200

    added active class on link navigation on link is active

--- react-routes/frontend/src/components/MainNavigation.js
+++ react-routes/frontend/src/components/MainNavigation.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
@@ -8,10 +8,19 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to='/'>Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to='/events'>Events</Link>
+            <NavLink to="/events" className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }>Events</NavLink>
           </li>
         </ul>
       </nav>
