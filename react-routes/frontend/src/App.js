5c900b693e311241fc76e1f712d35315b0f0ba0c
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-11 06:26:12 +0200

    added a end to home link

--- react-routes/frontend/src/App.js
+++ react-routes/frontend/src/App.js
@@ -17,7 +17,9 @@
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // Done
 // 4. Add properly working links to the MainNavigation
+//Done
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
+// Done
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
