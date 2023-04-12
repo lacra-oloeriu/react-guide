37fc502639ad5630ae13f468be774be4fbd23a23
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-12 07:37:40 +0200

    fixed bug

--- react-routes/frontend/src/App.js
+++ react-routes/frontend/src/App.js
@@ -25,7 +25,6 @@
 // Done
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
