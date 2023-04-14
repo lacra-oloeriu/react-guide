c9b2ede058bc06dfab7ff4b162979fa7d307f3e0
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-14 11:25:00 +0200

    removed the useNavigation

--- react-routes/frontend/src/pages/Root.js
+++ react-routes/frontend/src/pages/Root.js
@@ -1,14 +1,14 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
   //const events = useLoaderData();
-  const navigation = useNavigation();
+  //const navigation = useNavigation();
+  //{navigation.state === ' loading'&& <p>Loading...</p>}
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === ' loading'&& <p>Loading...</p>}
         <Outlet />
       </main>
     </>
