463d936d45c04e5adddebba5a8015b657b20a903
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-15 08:16:55 +0200

    returning resp[onse in loader

--- react-routes/frontend/src/pages/Events.js
+++ react-routes/frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function Events() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
   return <EventsList events={events} />;
 }
 
@@ -13,7 +14,6 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
