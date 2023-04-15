f2a1da7feb03bd2ac6e94bf73f0aef581f10d921
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-15 20:09:38 +0200

    handeling errors

--- react-routes/frontend/src/pages/Events.js
+++ react-routes/frontend/src/pages/Events.js
@@ -4,6 +4,9 @@ import EventsList from "../components/EventsList";
 
 function Events() {
   const data = useLoaderData();
+  //if ( data.isError) {
+  // return <p>{data.message}</p>
+  //}
   const events = data.events;
   return <EventsList events={events} />;
 }
@@ -11,8 +14,10 @@ function Events() {
 export default Events;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsdsfdasfs");
   if (!response.ok) {
+    // return { isError:true, message: 'Could not fetch events.'}
+    throw { message: "Could not fetch events." };
   } else {
     return response;
   }
