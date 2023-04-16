7b521d98cc50e9883b460df3f5b0ec8c234a1553
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-16 07:42:59 +0200

    fixed spelling on PageContent

--- react-routes/frontend/src/pages/Error.js
+++ react-routes/frontend/src/pages/Error.js
@@ -1,6 +1,6 @@
 import { useRouteError } from "react-router-dom";
 
-import PageContent from "../components/PaageContent";
+import PageContent from "../components/PageContent";
 import MainNavigation from "../components/MainNavigation";
 
 function ErrorPage() {
