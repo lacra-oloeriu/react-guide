100556c16ed6aaab19763b61de85df34a34a261d
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-20 13:58:24 +0200

    added a Newsletter page

--- react-routes/frontend/src/App.js
+++ react-routes/frontend/src/App.js
@@ -13,6 +13,7 @@ import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
@@ -49,6 +50,11 @@ const router = createBrowserRouter([
           },
           { path: "new", element: <NewEvent />, action: manipulateEventAction },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
