482b17172e556045ac2f3ae072b7a7717cb51607
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-20 11:04:37 +0200

    actions via request methosd

--- react-routes/frontend/src/pages/EditEvent.js
+++ react-routes/frontend/src/pages/EditEvent.js
@@ -1,11 +1,11 @@
-import { useRouteLoaderData} from 'react-router-dom';
+import { useRouteLoaderData } from "react-router-dom";
 
 import EventForm from "../components/EventForm";
 
 function EditEvent() {
- const data = useRouteLoaderData('event-detail');
- //const event = data.event;
-  return <EventForm  event = {data.event}/>;
+  const data = useRouteLoaderData("event-detail");
+  //const event = data.event;
+  return <EventForm method="patch" event={data.event} />;
 }
 
 export default EditEvent;
