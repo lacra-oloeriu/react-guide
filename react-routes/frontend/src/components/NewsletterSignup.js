1c03246e544c55bd928eb97c075dd93b488bcb44
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-21 13:45:07 +0200

    used the Fetcher hook

--- react-routes/frontend/src/components/NewsletterSignup.js
+++ react-routes/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,22 @@
-import classes from './NewsletterSignup.module.css';
+import { useFetcher } from "react-router-dom";
+
+import classes from "./NewsletterSignup.module.css";
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
