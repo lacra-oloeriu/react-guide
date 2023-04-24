fa807c941f1575e2695f02dc29cd7db24ff57f4d
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-24 13:47:09 +0200

    controling which Data should be deferred

--- react-routes/frontend/src/pages/Events.js
+++ react-routes/frontend/src/pages/Events.js
@@ -5,9 +5,7 @@ import EventsList from "../components/EventsList";
 
 function Events() {
   const { events } = useLoaderData();
-  //if ( data.isError) {
-  // return <p>{data.message}</p>
-  //}const events = data.events; return <EventsList events={events} />;
+  
   return (
     <Suspense fallback={<p style={{textAlign: 'center'}}>Loading ...</p>}>
       <Await resolve={events}>
@@ -21,21 +19,22 @@ function Events() {
 export default Events;
 
 async function loadEvents() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch('http://localhost:8080/events');
+
   if (!response.ok) {
-    // return { isError:true, message: 'Could not fetch events.'}
-    //throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-    //status: 500,
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
     // });
     throw json(
-      { message: "Could not fetch events." },
+      { message: 'Could not fetch events.' },
       {
         status: 500,
       }
     );
   } else {
     const resData = await response.json();
-    return resData.events
+    return resData.events;
   }
 }
 
