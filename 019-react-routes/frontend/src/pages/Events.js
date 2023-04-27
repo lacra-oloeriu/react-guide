350f61dbaebedd646ac557d9066005ab8a9e91a3
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-04-27 13:12:03 +0200

    output a list of dummy events that include a link

--- 019-react-routes/frontend/src/pages/Events.js
+++ 019-react-routes/frontend/src/pages/Events.js
@@ -1,5 +1,29 @@
-function EventsPage () {
-    return <hi>EventsPage</hi>
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Second event",
+  },
+];
+
+function EventsPage() {
+  return (
+    <>
+      <hi>EventsPage</hi>
+      <ul>
+        {DUMMY_EVENTS.map((event) => 
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
