9aa99e73b3075d9192f32fcb60ee1a8d0d0c9f1d
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-03 08:50:03 +0200

    fixed the path

--- 019-react-routes/frontend/src/pages/Events.js
+++ 019-react-routes/frontend/src/pages/Events.js
@@ -13,7 +13,7 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsdsdssfs");
+  const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
       status: 500,
