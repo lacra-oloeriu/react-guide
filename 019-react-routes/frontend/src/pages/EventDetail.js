baa27da7547b9a0b2b8201707a2aa17c53efd9ad
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-03 14:15:10 +0200

    loaded the datail data for an event

--- 019-react-routes/frontend/src/pages/EventDetail.js
+++ 019-react-routes/frontend/src/pages/EventDetail.js
@@ -1,11 +1,27 @@
-import { useParams} from 'react-router-dom';
+import EventItem from "../components/EventItem";
 
-function EventDetailPage () {
-    const params = useParams();
-    return <>
-    <hi>EventDetailPage</hi>
-    <p>Event ID: {params.eventId}</p>
-    </>
+import { useLoaderData, json } from "react-router-dom";
+
+function EventDetailPage() {
+  const data = useLoaderData();
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details fro selectet event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
