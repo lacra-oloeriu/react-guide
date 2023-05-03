87793e148462beb5662c69bdb8d1957259a91261
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-03 07:01:00 +0200

    extracted error data and throwing responses

--- 019-react-routes/frontend/src/pages/Error.js
+++ 019-react-routes/frontend/src/pages/Error.js
@@ -1,9 +1,24 @@
+import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse( error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not find!";
+    message = "Could not find resource or page";
+  }
+  
   return (
-    <PageContent title="An error occcurred!">
-      <p>Something went wrong!</p>
+    <PageContent title={title}>
+      <p>{message}</p>
     </PageContent>
   );
 }
