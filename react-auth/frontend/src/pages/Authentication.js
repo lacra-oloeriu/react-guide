0ea423ddc5aaedd9ef4eb58a8684be424ac6c8cb
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-10-11 11:00:22 +0200

    sended a http request to the backend

--- react-auth/frontend/src/pages/Authentication.js
+++ react-auth/frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -10,11 +11,21 @@ export async function action({request}) {
  const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get('mode') || 'login';
 
+if ( mode !=='login' && mode !== 'signup') {
+  throw  json ({message: 'Unsuported mode.'}, {status: 422})
+}
+
   const data = await request.form();
   const authData = {
       email: data.get('email'),
       password: data.get('password')
   };
 
-  fetch('http://localhost:8080/login'+ mode);
+   const respomse = fetch('http://localhost:8080/login'+ mode, {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(authData)
+   });
 }
