817c6beebabb52f1e7c8d12fec1a6304425c9007
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-10-31 15:51:04 +0100

    added some code to manage errors

--- tanstack-query/src/components/Events/NewEvent.jsx
+++ tanstack-query/src/components/Events/NewEvent.jsx
@@ -4,11 +4,12 @@ import { useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-   const { mutate } = useMutation ({
+   const { mutate, isPending, isError, error } = useMutation ({
     mutationFn: createNewEvent
   })
 
@@ -19,6 +20,8 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
+        {isPending && 'Submitting...'}
+        {!isPending && (
         <>
           <Link to="../" className="button-text">
             Cancel
@@ -27,7 +30,9 @@ export default function NewEvent() {
             Create
           </button>
         </>
+        )}
       </EventForm>
+      {isError && <ErrorBlock  title='Failed to create event .' message={error.info?.message || 'Failed to create event .Please check your inputs and try again later.'}/>}
     </Modal>
   );
 }
