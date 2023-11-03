28496ebcf97eb9b5b1f46f4eb1d8f29bcd141ae6
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-11-03 07:23:49 +0100

    added a delete and a cencel confirmation buttons

--- tanstack-query/src/components/Events/EventDetails.jsx
+++ tanstack-query/src/components/Events/EventDetails.jsx
@@ -4,8 +4,11 @@ import { useQuery, useMutation } from '@tanstack/react-query';
 import Header from '../Header.jsx';
 import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { useState} from 'react';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
    const params = useParams();
    const navigate = useNavigate();
 
@@ -24,6 +27,14 @@ export default function EventDetails() {
       navigate('/events');
     }
   });
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  };
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
 
   function handeleDelete() {
     mutate({ id: params.id});
@@ -56,7 +67,7 @@ export default function EventDetails() {
       <header>
           <h1>{data.title}</h1>
           <nav>
-            <button onClick={handeleDelete}>Delete</button>
+            <button onClick={handleStartDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
@@ -77,6 +88,16 @@ export default function EventDetails() {
 
   return (
     <>
+    {isDeleting && (
+    <Modal onClose={handleStopDelete}>
+      <h2>Are you sure?</h2>
+      <p>Do you really want to delete this event? This action cannot be undone.</p>
+      <div className='form-action'>
+        <button onClick={handleStopDelete} className='button-text'>Cancel</button>
+        <button onClick={handeleDelete} className='button'>Delete</button>
+      </div>
+    </Modal>
+    )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
